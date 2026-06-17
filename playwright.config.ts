e84c50a0dc5b309c6b3df7386f1b6fbe1d277d02
Christian Dye <christiancdye@gmail.com>
2026-06-17 14:50:00 -0500

    webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,6 @@ export default defineConfig({
     projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
     { name: 'firefox',  use: { ...devices['Desktop Firefox'] } },
-    { name: 'mobile',   use: { ...devices['iPhone 16'] } },
+    { name: 'webkit',   use: { ...devices['iPhone 16'] } },
   ],
 });
