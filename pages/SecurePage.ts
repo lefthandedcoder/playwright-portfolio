7b7c92a69f7f814dcc818c7527dd5b346b5e7ead
Christian Dye <christiancdye@gmail.com>
2026-06-17 12:44:35 -0500

    secure page

--- pages/SecurePage.ts
+++ pages/SecurePage.ts
@@ -2,37 +2,27 @@ import { Page, Locator, expect} from '@playwright/test'
 
 export class SecurePage {
     readonly page: Page;
-    readonly usernameInput: Locator;
-    readonly passwordInput: Locator;
-    readonly loginButton: Locator;
-    readonly flashMessage: Locator;
+    readonly messageAlert: Locator;
+    readonly logoutButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
-        this.usernameInput = page.locator('#username');
-        this.passwordInput = page.locator('#password');
-        this.loginButton = page.locator('button[type="submit"]')
-        this.flashMessage = page.locator('#flash');
+        this.messageAlert = page.locator('#flash');
+        this.logoutButton = page.locator('a.button.secondary')
     }
 
-    async goto() {
-        await this.page.goto('/login');
+    async logout() {
+        await this.logoutButton.click();
     }
 
-    async login(username: string, password: string) {
-        await this.usernameInput.fill(username);
-        await this.passwordInput.fill(password);
-        await this.loginButton.click();
-    }
-
-    async expectSuccessfulLogin() {
-        await expect(this.flashMessage).toContainText('You logged into a secure area!');
+    async expectLoaded() {
         await expect(this.page).toHaveURL('/secure');
+        await expect(this.logoutButton).toBeVisible();
     }
 
-    async expectLoginError() {
-        await expect(this.flashMessage).toBeVisible();
-        await expect(this.page).toHaveURL('/login');
+    async expectLoggedOut() {
+        await expect(this.messageAlert).toContainText('You logged out of the secure area!');
+        expect(this.page).toHaveURL('/login');
     }
 
 }
