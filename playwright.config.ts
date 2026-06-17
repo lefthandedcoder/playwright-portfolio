58d61bf0fbb48a51d76ca877bd4425ac6fca8e3d
Christian Dye <christiancdye@gmail.com>
2026-06-17 09:02:44 -0500

    Initial commit

---
+++ playwright.config.ts
@@ -0,0 +1,24 @@
+import {defineConfig, devices} from '@playwright/test'
+
+export default defineConfig({
+    testDir: './tests',
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 2 : undefined,
+    reporter: [
+        ['html', { outputFolder: 'playwright-report', open: 'never' }],
+        ['list'],
+        ['junit', { outputFile: 'test-results/results.xml'}]
+    ],
+    use: {
+        baseURL: 'https://the-internet.herokuapp.com',
+        trace: 'on-first-retry',
+        screenshot: 'only-on-failure',        
+    },
+    projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox',  use: { ...devices['Desktop Firefox'] } },
+    { name: 'mobile',   use: { ...devices['Pixel 5'] } },
+  ],
+});
